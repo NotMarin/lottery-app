6a36ef67faeea2cc6ff6a9f39a2f91176ea8e553
Santiago Marin <santimarinh04@gmail.com>
2026-04-17 15:39:59 -0500

    feat: initial config

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/src/lib/utils";
+import { StrictMode } from "react";
+
+const poppins = Poppins({
+  variable: "--font-sans",
+  weight: ["400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "El Chancerito | Gestión de Tickets de Lotería Online",
+  description:
+    "Gestiona tus tickets de lotería de forma rápida y segura. Registra números, edita jugadas, genera comprobantes en PDF, recibe sugerencias automáticas y organiza sorteos diarios de múltiples loterías en una sola plataforma.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="es"
+      className={cn("h-full", "antialiased", "font-sans", poppins.variable)}
+    >
+      <StrictMode>
+        <body className="flex min-h-full flex-col">{children}</body>
+      </StrictMode>
+    </html>
+  );
+}
